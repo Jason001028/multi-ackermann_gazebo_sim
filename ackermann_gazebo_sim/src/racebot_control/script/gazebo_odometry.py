c54a2cad8b8dfcde38b1790b02d0516001165699
Jason001028 <https://github.com/Jason001028>
2026-04-23 18:35:35 +0800

    修复bug，在wsl运行

--- ackermann_gazebo_sim/src/racebot_control/script/gazebo_odometry.py
+++ ackermann_gazebo_sim/src/racebot_control/script/gazebo_odometry.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 '''
-This script makes Gazebo less fail by translating gazebo status messages to odometry data.
-Since Gazebo also publishes data faster than normal odom data, this script caps the update to 20hz.
-Winter Guerra
+Translates Gazebo link states to per-robot odometry, capped at 20 Hz.
 '''
 
 import rospy
@@ -11,39 +9,34 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose, Twist, Transform, TransformStamped
 from gazebo_msgs.msg import LinkStates
 from std_msgs.msg import Header
-import numpy as np
-import math
 import tf2_ros
 
+
 class OdometryNode:
-    # Set publishers
-    pub_odom = rospy.Publisher('/odom', Odometry, queue_size=1)
 
     def __init__(self):
-        # init internals
+        self.robot_name = rospy.get_param('~robot_name', rospy.get_namespace().strip('/') or 'racebot')
+        self.link_name = self.robot_name + '::base_footprint'
+        self.odom_frame = self.robot_name + '/odom'
+        self.base_frame = self.robot_name + '/base_footprint'
+
         self.last_received_pose = Pose()
         self.last_received_twist = Twist()
         self.last_recieved_stamp = None
 
-        # Set the update rate
-        rospy.Timer(rospy.Duration(.05), self.timer_callback) # 20hz
-
+        self.pub_odom = rospy.Publisher('odom', Odometry, queue_size=1)
         self.tf_pub = tf2_ros.TransformBroadcaster()
 
-        # Set subscribers
+        rospy.Timer(rospy.Duration(.05), self.timer_callback)
         rospy.Subscriber('/gazebo/link_states', LinkStates, self.sub_robot_pose_update)
 
     def sub_robot_pose_update(self, msg):
-        # Find the index of the racecar
         try:
-            arrayIndex = msg.name.index('racebot::base_footprint')
-        except ValueError as e:
-            # Wait for Gazebo to startup
-            pass
-        else:
-            # Extract our current position information
-            self.last_received_pose = msg.pose[arrayIndex]
-            self.last_received_twist = msg.twist[arrayIndex]
+            idx = msg.name.index(self.link_name)
+        except ValueError:
+            return
+        self.last_received_pose = msg.pose[idx]
+        self.last_received_twist = msg.twist[idx]
         self.last_recieved_stamp = rospy.Time.now()
 
     def timer_callback(self, event):
@@ -52,33 +45,28 @@ class OdometryNode:
 
         cmd = Odometry()
         cmd.header.stamp = self.last_recieved_stamp
-        cmd.header.frame_id = 'odom'
-        cmd.child_frame_id = 'base_footprint'
+        cmd.header.frame_id = self.odom_frame
+        cmd.child_frame_id = self.base_frame
         cmd.pose.pose = self.last_received_pose
         cmd.twist.twist = self.last_received_twist
-        cmd.pose.covariance =[1e-3, 0, 0, 0, 0, 0,
-						0, 1e-3, 0, 0, 0, 0,
-						0, 0, 1e6, 0, 0, 0,
-						0, 0, 0, 1e6, 0, 0,
-						0, 0, 0, 0, 1e6, 0,
-						0, 0, 0, 0, 0, 1e3]
-
-        cmd.twist.covariance = [1e-9, 0, 0, 0, 0, 0, 
-                          0, 1e-3, 1e-9, 0, 0, 0,
-                          0, 0, 1e6, 0, 0, 0,
-                          0, 0, 0, 1e6, 0, 0,
-                          0, 0, 0, 0, 1e6, 0,
-                          0, 0, 0, 0, 0, 1e-9]
-
+        cmd.pose.covariance = [1e-3, 0, 0, 0, 0, 0,
+                               0, 1e-3, 0, 0, 0, 0,
+                               0, 0, 1e6, 0, 0, 0,
+                               0, 0, 0, 1e6, 0, 0,
+                               0, 0, 0, 0, 1e6, 0,
+                               0, 0, 0, 0, 0, 1e3]
+        cmd.twist.covariance = [1e-9, 0, 0, 0, 0, 0,
+                                0, 1e-3, 1e-9, 0, 0, 0,
+                                0, 0, 1e6, 0, 0, 0,
+                                0, 0, 0, 1e6, 0, 0,
+                                0, 0, 0, 0, 1e6, 0,
+                                0, 0, 0, 0, 0, 1e-9]
 
         self.pub_odom.publish(cmd)
 
         tf = TransformStamped(
-            header=Header(
-                frame_id=cmd.header.frame_id,
-                stamp=cmd.header.stamp
-            ),
-            child_frame_id=cmd.child_frame_id,
+            header=Header(frame_id=self.odom_frame, stamp=cmd.header.stamp),
+            child_frame_id=self.base_frame,
             transform=Transform(
                 translation=cmd.pose.pose.position,
                 rotation=cmd.pose.pose.orientation
@@ -86,8 +74,8 @@ class OdometryNode:
         )
         self.tf_pub.sendTransform(tf)
 
-# Start the node
+
 if __name__ == '__main__':
-    rospy.init_node("gazebo_odometry_node")
+    rospy.init_node('gazebo_odometry_node')
     node = OdometryNode()
     rospy.spin()
--- ackermann_gazebo_sim/src/racebot_control/script/servo_commands.py
+++ ackermann_gazebo_sim/src/racebot_control/script/servo_commands.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Bool
-from std_msgs.msg import Float32
 from std_msgs.msg import Float64
 from ackermann_msgs.msg import AckermannDriveStamped
 
-flag_move = 0
+
+def make_publishers(ns):
+    return {
+        'lrw': rospy.Publisher(ns + '/left_rear_wheel_velocity_controller/command', Float64, queue_size=1),
+        'rrw': rospy.Publisher(ns + '/right_rear_wheel_velocity_controller/command', Float64, queue_size=1),
+        'lfw': rospy.Publisher(ns + '/left_front_wheel_velocity_controller/command', Float64, queue_size=1),
+        'rfw': rospy.Publisher(ns + '/right_front_wheel_velocity_controller/command', Float64, queue_size=1),
+        'lsh': rospy.Publisher(ns + '/left_steering_hinge_position_controller/command', Float64, queue_size=1),
+        'rsh': rospy.Publisher(ns + '/right_steering_hinge_position_controller/command', Float64, queue_size=1),
+    }
+
+
+pubs = {}
+
 
 def set_throttle_steer(data):
-
-    global flag_move
-
-    pub_vel_left_rear_wheel = rospy.Publisher('/racebot/left_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_rear_wheel = rospy.Publisher('/racebot/right_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_left_front_wheel = rospy.Publisher('/racebot/left_front_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_front_wheel = rospy.Publisher('/racebot/right_front_wheel_velocity_controller/command', Float64, queue_size=1)
-
-    pub_pos_left_steering_hinge = rospy.Publisher('/racebot/left_steering_hinge_position_controller/command', Float64, queue_size=1)
-    pub_pos_right_steering_hinge = rospy.Publisher('/racebot/right_steering_hinge_position_controller/command', Float64, queue_size=1)
-
-    throttle = data.drive.speed*31.25
+    throttle = data.drive.speed * 31.25
     steer = data.drive.steering_angle
+    pubs['lrw'].publish(throttle)
+    pubs['rrw'].publish(throttle)
+    pubs['lfw'].publish(throttle)
+    pubs['rfw'].publish(throttle)
+    pubs['lsh'].publish(steer)
+    pubs['rsh'].publish(steer)
 
-    pub_vel_left_rear_wheel.publish(throttle)
-    pub_vel_right_rear_wheel.publish(throttle)
-    pub_vel_left_front_wheel.publish(throttle)
-    pub_vel_right_front_wheel.publish(throttle)
-    pub_pos_left_steering_hinge.publish(steer)
-    pub_pos_right_steering_hinge.publish(steer)
 
 def servo_commands():
-
+    global pubs
     rospy.init_node('servo_commands', anonymous=True)
 
-    rospy.Subscriber("/ackermann_cmd_mux/output", AckermannDriveStamped, set_throttle_steer)
+    robot_name = rospy.get_param('~robot_name', rospy.get_namespace().strip('/') or 'racebot')
+    ns = '/' + robot_name
 
-    # spin() simply keeps python from exiting until this node is stopped
+    pubs = make_publishers(ns)
+
+    rospy.Subscriber(ns + '/ackermann_cmd', AckermannDriveStamped, set_throttle_steer)
     rospy.spin()
+
 
 if __name__ == '__main__':
     try:
